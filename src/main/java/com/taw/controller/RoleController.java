83f1c8da862a07c6250f9fdfae92c8eaeb5c2542
AowenTan <429762561@qq.com>
2019-10-08 00:07:00 +0800

    role初建立，未完成

---
+++ src/main/java/com/taw/controller/RoleController.java
@@ -0,0 +1,14 @@
+package com.taw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/role")
+public class RoleController {
+
+    @RequestMapping("/findAll.do")
+    public void findAll(){
+
+    }
+}
